42a950eb01bb5e278958d0ce13bd4db15afd5b59
Benjamin Moir <DaZombieKiller@users.noreply.github.com>
2025-12-30 19:51:36 +1000

    Add AnimationClip API bridges

---
+++ .Source/PrivateAPIEngineBridge/AnimationClipExtensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace PrivateAPIBridge;
+
+/// <summary>Provides extensions for <see cref="AnimationClip"/>.</summary>
+public static class AnimationClipExtensions
+{
+    extension(AnimationClip @this)
+    {
+        public float startTime => @this.startTime;
+
+        public float stopTime => @this.stopTime;
+
+        public bool hasRootMotion => @this.hasRootMotion;
+    }
+}
---
+++ .Source/UnityEngine/AnimationClip.cs
@@ -0,0 +1,10 @@
+﻿namespace UnityEngine;
+
+public sealed class AnimationClip : Motion
+{
+    public extern float startTime { get; }
+
+    public extern float stopTime { get; }
+
+    public bool hasRootMotion { get; }
+}

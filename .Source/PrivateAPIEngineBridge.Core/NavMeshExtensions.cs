5024b2c70db517332f66aa8c0912711bd462edb6
Benjamin Moir <DaZombieKiller@users.noreply.github.com>
2026-02-25 07:02:47 +1000

    Add some NavMesh APIs

---
+++ .Source/PrivateAPIEngineBridge.Core/NavMeshExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace PrivateAPIBridge;
+
+/// <summary>Provides extensions for <see cref="NavMesh"/>.</summary>
+public static class NavMeshExtensions
+{
+    extension(NavMesh)
+    {
+        /// <summary>Returns an existing entry of NavMesh build settings by its ordered index.</summary>
+        public static void GetSettingsByIndex_Injected(int index, out NavMeshBuildSettings ret) => NavMesh.GetSettingsByIndex_Injected(index, out ret);
+
+        /// <summary>Adds a link to the NavMesh. The link is described by the NavMeshLinkData struct.</summary>
+        public static int AddLinkInternal_Injected(ref NavMeshLinkData link, ref Vector3 position, ref Quaternion rotation) => NavMesh.AddLinkInternal_Injected(ref link, ref position, ref rotation);
+    }
+}
---
+++ .Source/UnityEngine/NavMesh.cs
@@ -0,0 +1,8 @@
+﻿namespace UnityEngine.AI;
+
+public static class NavMesh
+{
+    public static extern void GetSettingsByIndex_Injected(int index, out NavMeshBuildSettings ret);
+
+    public static extern int AddLinkInternal_Injected(ref NavMeshLinkData link, ref Vector3 position, ref Quaternion rotation);
+}
